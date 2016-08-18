4726663773a69ec9ed46482794d530a0c2f92c18
mounika2525 <mounika123.turlapate@gmail.com>
2016-08-18 17:55:55 +0530

    Login controller

--- src/main/java/com/common/Controller/LoginController.java
+++ src/main/java/com/common/Controller/LoginController.java
@@ -3,6 +3,8 @@ package com.common.Controller;
 import com.common.Service.LoginService;
 import com.common.model.Login;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,29 +31,39 @@ public class LoginController {
 //        else throw new HandlerException("no such user",new RuntimeException());
 //    }
 
-    @RequestMapping(value = "/credential/", method = RequestMethod.POST)
-    public List<Login> getByUseNameAndPassword(@RequestBody Login login) {
-        try {
+   // @RequestMapping(value = "/credential/", method = RequestMethod.PUT)
+//    public List<Login> getByUseNameAndPassword(@RequestBody Login login) {
+//        try {
+//
+//            String user = login.getUsername();
+//            String pass = login.getPassword();
+////            List<Login> loginser = new ArrayList<Login>();
+////            loginser.add(login);
+//            List<Login> login1 = loginservice.findByusernameAndPassword(user, pass);
+//            for (Login login2 : login1) {
+//                if (login2.getUsername().equals(user) && login2.getPassword().equals(pass))
+//                    return login1;
+//                else
+//                    return null;
+//            }
+//        } catch (Exception e) {
+//            return null;
+//        }
+//        return null;
+//    }
 
-            String user = login.getUsername();
-            String pass = login.getPassword();
-//            List<Login> loginser = new ArrayList<Login>();
-//            loginser.add(login);
-            List<Login> login1 = loginservice.findByusernameAndPassword(user, pass);
-            for (Login login2 : login1) {
-                if (login2.getUsername().equals(user) && login2.getPassword().equals(pass))
-                    return login1;
-                else
-                    return null;
-            }
-        } catch (Exception e) {
-            return null;
+    @RequestMapping(value = "/credential/", method = RequestMethod.GET)
+public ResponseEntity<List<Login>> getByUseNameAndPassword(@RequestParam String username,@RequestParam String password)
+    {
+
+
+        List<Login> logins=loginservice.findByusernameAndPassword(username,password);
+        if(logins.isEmpty()) {
+            return new ResponseEntity<List<Login>>(HttpStatus.OK);
+
         }
-        return null;
+        return new ResponseEntity<List<Login>>(logins, HttpStatus.OK);
     }
-
-
-
 
 }
 
